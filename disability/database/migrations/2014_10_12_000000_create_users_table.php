f05c7b856e4f01500e896bf87558fe8cf3e9d119
shashankj99 <shashankj677@gmail.com>
2020-07-30 11:22:28 +0545

    DB issue resolved

--- disability/database/migrations/2014_10_12_000000_create_users_table.php
+++ disability/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,12 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->charset('utf8')->collation('utf8_unicode_ci');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('phone')->charset('utf8')->collation('utf8_unicode_ci')->nullable();
+            $table->string('address')->charset('utf8')->collation('utf8_unicode_ci')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
