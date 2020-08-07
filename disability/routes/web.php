9f782e315766a5d0f9b94159d5bad5e1ac0b0512
shashankj99 <shashankj677@gmail.com>
2020-08-07 15:29:18 +0545

    disable register function

--- disability/routes/web.php
+++ disability/routes/web.php
@@ -7,7 +7,7 @@ Route::get('/', function () {
     return redirect()->route('home');
 });
 
-Auth::routes();
+Auth::routes(['register' => false]);
 
 Route::get('/home', 'HomeController@index')->name('home');
 
